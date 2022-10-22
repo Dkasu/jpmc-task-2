249599bfd9b406f7676050cf820d405c907dd827
dkasu <danyalkasu1@gmail.com>
2022-10-22 13:15:44 +0100

    nice

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -49,8 +49,8 @@ class Graph extends Component<IProps, {}> {
         elem.setAttribute('view', 'y_line');
         elem.setAttribute('column-pivots', '["stock"]');
         elem.setAttribute('row-pivots', '["timestamp"]');
-        elem.setAttribute('columns', ["top_ask_price"]);
-        elem.setAttribute('aggregates'`
+        elem.setAttribute('columns', '["top_ask_price"]');
+        elem.setAttribute('aggregates',`
         {"stock":"distinct count",
         "top_ask_price":"avg",
         "top_bid_price":avg,
